6aefd6b2b5f085e2d7e3a7abdd56ef026f3a885c
Anurag Singh <94917640+Anurag0577@users.noreply.github.com>
2023-06-16 14:43:31 +0530

    first commit

---
+++ employee-management-system/src/main/java/com/myproject/employee/services/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.myproject.employee.services;
+
+import com.myproject.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+}
